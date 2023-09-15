578d553cd7f266e712ad7be8159aa69b10ead6cb
BRUNO ROGLIN <49683237+epifano-x@users.noreply.github.com>
2023-09-15 14:26:41 -0300

    entrega 3 finalizada

--- src/main/java/br/edu/utfpr/turismoapi/controllers/AuthController.java
+++ src/main/java/br/edu/utfpr/turismoapi/controllers/AuthController.java
@@ -1,24 +1,27 @@
 package br.edu.utfpr.turismoapi.controllers;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.utfpr.turismoapi.dto.AuthDTO;
 import br.edu.utfpr.turismoapi.security.JwtUtil;
 import jakarta.validation.Valid;
 
+@RestController
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
     private JwtUtil jwtUtil;
 
+    @PostMapping
     public ResponseEntity<Object> auth(@Valid @RequestBody AuthDTO authDTO) {
         System.out.println(authDTO);
 
